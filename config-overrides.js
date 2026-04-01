92978eb57916576b3face8b5fdb9cd99e10e362b
abhishektopu <105499232+abhishektopu@users.noreply.github.com>
2026-04-02 01:54:35 +0530

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,20 @@
+const webpack = require("webpack");
+
 module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
     stream: require.resolve("stream-browserify"),
     crypto: require.resolve("crypto-browserify"),
     buffer: require.resolve("buffer"),
-    util: require.resolve("util/")
   };
+
+  config.plugins = [
+    ...(config.plugins || []),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ];
+
   return config;
 };
