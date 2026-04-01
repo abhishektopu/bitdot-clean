8c339ed28809bf1ef11d60ef10bf49c062dfbd79
abhishektopu <105499232+abhishektopu@users.noreply.github.com>
2026-04-01 22:39:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 
 
 // ✅ FACEBOOK PIXEL (CORRECT WAY)
-!function(f,b,e,v,n,t,s)
+// FACEBOOK PIXEL
+(function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
+if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];
+t=b.createElement(e);t.async=true;
+t.src=v;
+s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)})(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-
 // 👉 YOUR PIXEL ID
 fbq('init', '1636533930618738');
 
