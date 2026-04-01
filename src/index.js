49d81b994d9ef0b4ee6314190070ae460078c178
abhishektopu <105499232+abhishektopu@users.noreply.github.com>
2026-04-01 23:30:46 +0530

    Fix stream polyfill issue for Cloudflare build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
+import { Buffer } from "buffer";
+window.Buffer = Buffer;
 
 
 // ✅ FACEBOOK PIXEL (CORRECT WAY)
