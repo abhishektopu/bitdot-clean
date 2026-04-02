1570d4b489b0617442e36157482cc429fcfd7b57
abhishektopu <105499232+abhishektopu@users.noreply.github.com>
2026-04-02 11:17:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,24 +5,6 @@ import { Buffer } from "buffer";
 window.Buffer = Buffer;
 
 
-// ✅ FACEBOOK PIXEL (CORRECT WAY)
-// FACEBOOK PIXEL
-(function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;
-n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];
-t=b.createElement(e);t.async=true;
-t.src=v;
-s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)})(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-// 👉 YOUR PIXEL ID
-fbq('init', '1636533930618738');
-
-// 👉 TRACK PAGE VIEW
-fbq('track', 'PageView');
 
 
 import App from './App';
